381fe5b1cae19147547be196525a84f8543ca60a
Роман Вайсер <immdevrov@gmail.com>
2020-01-03 16:33:29 +0300

    Добравил компонент возможных результатов поиска

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,15 +7,13 @@
         class="input"
         @input="onInput"
       />
-      <div class="suggestions-block">
-        <div
-          class="suggestion"
-          v-for="(suggestion, index) in suggestions"
-          :key="index"
-        >
-          {{suggestion}}
-        </div>
-      </div>
+    </div>
+    <div class="search__suggestion">
+      <Suggestion
+        v-for="(suggestion, index) in suggestions"
+        :key="index"
+        :film="suggestion"
+      />
     </div>
     <button
       @click="test"
@@ -30,31 +28,67 @@
 
 <script>
 import axios from "axios";
+import Suggestion from "./Suggestion"
 import { testPath, requestSearch } from "../assets/js/api";
 
 export default {
+  components: {
+    Suggestion
+  },
   data() {
     return {
       search: "",
-      suggestions: []
+      suggestions: [
+        {
+          title: 'The Guard',
+          year: '2011',
+          imdbID: 'tt4938602',
+          type: 'movie',
+          poster: 'https://m.media-amazon.com/images/M/MV5BMTY2ODkzMDgwM15BMl5BanBnXkFtZTcwMDA1Mjg1OA@@._V1_SX300.jpg'
+        },
+        {
+          title: 'The Lion Guard',
+          year: '2016–',
+          imdbID: 'tt3793630',
+          type: 'series',
+          poster: 'https://m.media-amazon.com/images/M/MV5BMDNkZWIyN2EtOTIxMi00NTYzLTg2MGUtOTRhNDMwMzM3M2VmXkEyXkFqcGdeQXVyMDM0MzU2NA@@._V1_SX300.jpg'
+        },
+        {
+          title: 'Guard Dog',
+          year: '2004',
+          imdbID: 'tt0430159',
+          type: 'movie',
+          poster: 'https://m.media-amazon.com/images/M/MV5BMGUyNDNiMjEtYjc5Mi00MjYzLWEzZWYtYzM0NTVjYzI5Mzk0XkEyXkFqcGdeQXVyMTg2NzgzMDE@._V1_SX300.jpg'
+        },
+        {
+          title: 'Eklavya: The Royal Guard',
+          year: '2007',
+          imdbID: 'tt0459605',
+          type: 'movie',
+          poster: 'https://m.media-amazon.com/images/M/MV5BM2FiM2YzMGMtNzhhZS00N2E3LWI3MDEtNzAxODIxNWY5ZTkzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg'
+        }
+      ]
     };
   },
   methods: {
+    setLowerKeys (obj) {
+      let obj2 = {};
+      for (let index in obj) {
+        obj2[index.toLowerCase()] = obj[index];
+      }
+      return obj2;
+    },
     async onInput() {
+      this.setLowerKeys({Test: 'test'});
       const { data, meta } = await requestSearch(this.search);
-      if (data.Response === "True") {
-        this.suggestions = data.Search.map(film => film.Title);
+      if (data.Response === 'True') {
+        this.suggestions = data.Search.map(film => this.setLowerKeys(film))
+      } else {
+        this.suggestions = ''
       }
     },
     test() {
-      axios
-        .get(testPath)
-        .then(result => {
-          console.log(result);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      console.log('test');
     }
   }
 };
---
+++ src/components/Suggestion.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="suggestion">
+    <div class="suggestion__img img-wrapper">
+      <img :src="film.poster" alt="poster" class="img-r">
+    </div>
+    <div class="suggestion__name-block">
+      <div class="suggestion__name">
+        {{ film.title }}
+      </div>
+      <div class="suggestion__type">
+        {{ film.type }}
+      </div>
+    </div>
+    <div class="suggestion__year">
+      {{ film.year}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    film: {
+      type: Object,
+      value: ''
+    }
+  }
+}
+</script>
